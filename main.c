fd485b3aee828e3814c5c02b8b45798e7cd89634
Gastoncks <gastonvelperret@gmail.com>
2025-12-28 16:51:23 +0100

    Adding a 2nd window + gitignore

--- main.c
+++ main.c
@@ -8,44 +8,65 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Fenetre
-    SDL_Window* window = SDL_CreateWindow(
-        "GastonPaint",        // Titre
-        SDL_WINDOWPOS_CENTERED,     // Position X
-        SDL_WINDOWPOS_CENTERED,     // Position Y
-        800,                        // Largeur
-        600,                        // Hauteur
-        SDL_WINDOW_SHOWN            // Flags
-    );
-    if (!window) {
-        printf("Erreur de création de fenêtre: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    typedef struct {
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        Uint32 windowID;
+        bool mousePressed;
+        SDL_Rect pen;
+        int mouseX;
+        int mouseY;
+    } PaintWindow;
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    PaintWindow win1;
+    PaintWindow win2;
+
+    unsigned int wightpen = 10;
+
+    // Fenêtre 1
+    win1.window = SDL_CreateWindow(
+        "GastonPaint",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, SDL_WINDOW_SHOWN
+    );
+    win1.renderer = SDL_CreateRenderer(win1.window, -1, SDL_RENDERER_ACCELERATED);
+    win1.windowID = SDL_GetWindowID(win1.window);
+    win1.mousePressed = false;
+    win1.pen = (SDL_Rect){0, 0, wightpen, wightpen};
+    win1.mouseX = 0;
+    win1.mouseY = 0;
+
+    // Fenêtre 2
+    win2.window = SDL_CreateWindow(
+        "Couleur",
+        SDL_WINDOWPOS_CENTERED + 50, SDL_WINDOWPOS_CENTERED + 50,
+        400, 300, SDL_WINDOW_SHOWN
+    );
+    win2.renderer = SDL_CreateRenderer(win2.window, -1, SDL_RENDERER_ACCELERATED);
+    win2.windowID = SDL_GetWindowID(win2.window);
+    win2.mousePressed = false;
+    win2.pen = (SDL_Rect){0, 0, wightpen, wightpen};
 
     int running = 1;
     SDL_Event event;
 
-    int mouseX = 0;
-    int mouseY = 0;
-    unsigned int wightpen = 10;
-    bool mousePressed = false;
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(win1.renderer, 255, 255, 255, 255);
+    SDL_RenderClear(win1.renderer);
+    SDL_SetRenderDrawColor(win1.renderer, 0, 0, 0, 255);
     SDL_Rect rect = {0, 0, wightpen, wightpen};
-    SDL_RenderFillRect(renderer, &rect);
-    SDL_RenderDrawPoint(renderer, mouseX, mouseY);
+    SDL_RenderFillRect(win1.renderer, &win1.pen);
+    SDL_RenderDrawPoint(win1.renderer, win1.mouseX, win1.mouseY);
 
     while (running) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = 0;
+            if (event.type == SDL_WINDOWEVENT) {
+                if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+                    running = 0;
+                }
+            }
             if (event.type == SDL_MOUSEBUTTONDOWN) {
                 if (event.button.button == SDL_BUTTON_LEFT) {
-                    mousePressed = true;
+                    win1.mousePressed = true;
                 }
             }
             if (event.type == SDL_MOUSEWHEEL) {
@@ -59,24 +80,26 @@ int main(int argc, char* argv[]) {
             }
             if (event.type == SDL_MOUSEBUTTONUP) {
                 if (event.button.button == SDL_BUTTON_LEFT) {
-                    mousePressed = false;
+                    win1.mousePressed = false;
                 }
             }
             if (event.type == SDL_MOUSEMOTION) {
                 rect.x = event.motion.x - rect.w / 2;
                 rect.y = event.motion.y - rect.h / 2;
-                if (mousePressed) {
-                    SDL_RenderFillRect(renderer, &rect);
+                if (win1.mousePressed) {
+                    SDL_RenderFillRect(win1.renderer, &rect);
                 }
             }
         }
 
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(win1.renderer);
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(win1.renderer);
+    SDL_DestroyWindow(win1.window);
     SDL_Quit();
-
+    SDL_DestroyRenderer(win2.renderer);
+    SDL_DestroyWindow(win2.window);
+    SDL_Quit();
     return 0;
 }
